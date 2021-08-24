974d6cfc48d647ce0dfae993a8a95f6b38ae8ea7
Jeffery Le <jefferyle02@gmail.com>
2021-08-24 02:14:05 -0400

    bug fixes

--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
 // A Project by Jeffery Le
 
 import {doEDMAS} from './modules/operations.js'
-import * as say from './modules/input.js'
+import  {numberInp, operatorInp, brackInp, evalInp, clearDisplay, backspace} from './modules/input.js'
 
 
 //DECLARATIONS AND ASSIGNMENTS
