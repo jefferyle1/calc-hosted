3012bc9583fc07a92c5b936143ede3358fc1b3d8
Jeffery Le <jefferyle02@gmail.com>
2021-08-24 02:07:29 -0400

    reorganized files

--- calc.js
+++ calc.js
@@ -1,231 +1,7 @@
 // A Project by Jeffery Le
 
-//FUNCTIONS
-function add(a, b) {
-  a = Number(a);
-  b = Number(b);
-  return a + b;
-}
-
-function subtract(a, b) {
-  a = Number(a);
-  b = Number(b);
-  return a - b;
-}
-
-function multiply(a, b) {
-  a = Number(a);
-  b = Number(b);
-  return a * b;
-}
-
-function divide(a, b) {
-  a = Number(a);
-  b = Number(b);
-  return a / b;
-}
-
-function power(a, b) {
-  a = Number(a);
-  b = Number(b);
-  return Math.pow(a, b);
-}
-
-function doEDMAS(arr) {
-
-  let tempValue = 0;
-
-  //EXPONENT
-  while (arr.indexOf("^") != -1) {
-    let i = arr.indexOf("^");
-    tempValue = operate(arr[i - 1], arr[i], arr[i + 1]);
-    arr.splice(i - 1, 3, tempValue);
-  }
-
-  //DIVISION AND MULTIPLICATION
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == "x" || arr[i] == "÷") {
-      tempValue = operate(arr[i - 1], arr[i], arr[i + 1]);
-      arr.splice(i - 1, 3, tempValue);
-      i = i - 1;
-    }
-  }
-
-  //ADDITION AND SUBTRACTION
-  while (arr.length != 1) {
-    tempValue = operate(arr[0], arr[1], arr[2]);
-    arr.splice(0, 3, tempValue);
-  }
-
-  return arr[0].toString();
-
-}
-
-function operate(a, operator, b) {
-  return (operator == "+") ? add(a, b) :
-    (operator == "-") ? subtract(a, b) :
-      (operator == "x") ? multiply(a, b) :
-        (operator == "÷") ? divide(a, b) :
-          (operator == "^") ? power(a, b) :
-            "Error";
-}
-
-function numberInp(num) {
-  currDisplay = document.getElementById("display");
-
-  //WIPES DISPLAY
-  if (wipeable == true) {
-    clearDisplay();
-    wipeable = false;
-  }
-
-  //WIPES DISPLAY
-  if (wipeList.includes(currDisplay.innerHTML) && num != ".") {
-    currDisplay.innerHTML = "";
-  }
-
-  //GETS LAST CHARACTER IN THE DISPLAY
-  let trimmedDisplay = currDisplay.innerHTML.replace(/\s/g, '');
-  lastChar = trimmedDisplay.charAt(trimmedDisplay.length - 1);
-
-  //ADDS NUMBER TO DISPLAY
-  if (!(num == "." && lastChar == ".")) {
-    currDisplay.innerHTML = currDisplay.innerHTML + num;
-  }
-}
-
-function operator(op) {
-  currDisplay = document.getElementById("display");
-
-  wipeable = false;
-
-  //WIPES DISPLAY
-  if (wipeList.includes(currDisplay.innerHTML) && op == "-") {
-    currDisplay.innerHTML = "";
-  }
-
-  //GETS LAST CHARACTER
-  let trimmedDisplay = currDisplay.innerHTML.replace(/\s/g, '');
-  lastChar = trimmedDisplay.charAt(trimmedDisplay.length - 1);
-
-  //ADDS OPERATOR TO DISPLAY
-  if ((op == "-") && ((possibleOperators.includes(lastChar) || currDisplay.innerHTML == ""))) {
-    currDisplay.innerHTML = currDisplay.innerHTML + " " + op;
-  } else if ((isNaN(lastChar) == false || nonNumExceptions.includes(lastChar)) && currDisplay.innerHTML != "") {
-    currDisplay.innerHTML = currDisplay.innerHTML + " " + op + " ";
-  }
-}
-
-function brackInp (brack) { 
-  currDisplay = document.getElementById("display");
-
-    //WIPES DISPLAY
-    if (wipeable == true) {
-      clearDisplay();
-      wipeable = false;
-    }
-
-    //WIPES DISPLAY
-    if (wipeList.includes(currDisplay.innerHTML)) {
-      currDisplay.innerHTML = "";
-    }
-
-    //ADDS BRACKET TO DISPLAY
-    currDisplay.innerHTML = currDisplay.innerHTML + " " + brack + " ";
-}
-
-function eval() {
-  currDisplay = document.getElementById("display");
-
-  //REMOVES EMPTY STRINGS FROM ARRAY
-  let calcMemory = currDisplay.innerHTML.trim().split(" ");
-  calcMemory = calcMemory.filter(function (el) {
-    return el != "";
-  });
-
-  //ASSIGNMENTS 
-  let brackL = 0;
-  let brackR = 0;
-  let i = -1;
-  let errorPresent = false;
-
-  //WHILE LOOP THAT STOPS WHEN THERE'S NO MORE BRACKETS OR ERROR
-  while (calcMemory.includes("(") && (errorPresent == false)) {
-    i++;
-    //GETS POS OF LEFT BRACKET THAT'S CLOSEST TO A RIGHT ONE
-    if (calcMemory[i] == "(") {
-      brackL = i;
-      //GETS POS OF FIRST RIGHT BRACKET AND DOES CALCULATIONS
-    } else if (calcMemory[i] == ")") {
-      brackR = i;
-      //ARRAY IS CREATED TO PERFORM EDMAS ON THE VALUES INSIDE THE BRACKETS UNTIL IT'S REDUCED TO SINGLE VALUE
-      tempArray = calcMemory.slice(brackL + 1, brackR);
-      let deleteLength = brackR - brackL + 1
-      let finVal = doEDMAS(tempArray);
-
-      calcMemory.splice(brackL, deleteLength, finVal);
-
-      //STARTS SEARCH FOR BRACKETS AGAINT FROM ZERO
-      i = -1;
-    }
-
-    //DISPLAYS ERROR WHEN THERE'S AN INFITE LOOP
-    if (i > 99) {
-      errorPresent = true;
-    }
-
-  }
-
-  //DISPLAYS FLOAT OR INTEGER
-  if (doEDMAS(calcMemory).includes(".") == true) {
-    currDisplay.innerHTML = parseFloat(Number(doEDMAS(calcMemory)).toFixed(9));
-  } else {
-    currDisplay.innerHTML = doEDMAS(calcMemory);
-  }
-
-  //IF A NON-NUMBER IS PRODUCED, ERROR IS SHOWN
-  if (isNaN(currDisplay.innerHTML) == true) {
-    currDisplay.innerHTML = "ERROR"
-  }
-
-  wipeable = true;
-}
-
-function clearDisplay() {
-  document.getElementById("display").innerHTML = "0";
-}
-
-function backspace() {
-  let currDisplay = document.getElementById("display");
-
-  //GETS LAST CHAR
-  let trimmedDisplay = currDisplay.innerHTML.replace(/\s/g, '');
-  lastChar = trimmedDisplay.charAt(trimmedDisplay.length - 1);
-
-  //TRIMS DISPLAY IF THERE'S A SPACE
-  if (lastChar = " ") {
-    currDisplay.innerHTML = currDisplay.innerHTML.trim();
-  }
-  //WIPES DISPLAY
-  if (wipeable == true) {
-    clearDisplay();
-    wipeable = false;
-  }
-
-  //WIPES DISPLAY
-  if (wipeList.includes(currDisplay.innerHTML)) {
-    currDisplay.innerHTML = "0";
-  }
-
-  //REMOVES LAST CHARACTER
-  console.log(currDisplay.innerlength)
-  if (currDisplay.innerHTML.length == 1) {
-    currDisplay.innerHTML = "0";
-  } else if (currDisplay.innerHTML != "0") {
-    currDisplay.innerHTML = currDisplay.innerHTML.slice(0, currDisplay.innerHTML.length - 1);
-  }
-
-}
+import {doEDMAS} from './modules/operations.js'
+import * as say from './modules/input.js'
 
 
 //DECLARATIONS AND ASSIGNMENTS
@@ -241,16 +17,16 @@ document.addEventListener("keypress", function (event) {
   var inp = String.fromCharCode(keyC);
   let disp = document.getElementById("display").innerHTML
   if (event.key == "Enter" || inp == "=") {
-    eval();
+    evalInp();
   }
   else if (inp == ")" || inp == "(") {
     brackInp(inp);
   } else if (possibleOperators.includes(inp) == true) {
-    operator(inp);
+    operatorInp(inp);
   } else if (inp == "X" || inp == "*") {
-    operator("x");
+    operatorInp("x");
   } else if (inp == "/") {
-    operator("÷");
+    operatorInp("÷");
   } else if (isNaN(inp) == false || inp == ".") {
     numberInp(inp);
   } else if (event.keyCode == 127) { 
@@ -278,7 +54,7 @@ document.querySelectorAll('.number').forEach(numButt => {
 // EVENT LISTENER FOR OPERATORS
 document.querySelectorAll('.operator').forEach(opButt => {
   opButt.addEventListener("click", event => {
-    operator(opButt.innerHTML);
+    operatorInp(opButt.innerHTML);
   })
 })
 
@@ -292,7 +68,7 @@ document.querySelectorAll('.bracket').forEach(brButt => {
 
 // EVENT LISTENER FOR EQUALS
 document.querySelector('.equals').addEventListener("click", event => {
-  eval();
+  evalInp();
 })
 
 
---
+++ modules/input.js
@@ -0,0 +1,156 @@
+export function numberInp(num) {
+  currDisplay = document.getElementById("display");
+
+  //WIPES DISPLAY
+  if (wipeable == true) {
+    clearDisplay();
+    wipeable = false;
+  }
+
+  //WIPES DISPLAY
+  if (wipeList.includes(currDisplay.innerHTML) && num != ".") {
+    currDisplay.innerHTML = "";
+  }
+
+  //GETS LAST CHARACTER IN THE DISPLAY
+  let trimmedDisplay = currDisplay.innerHTML.replace(/\s/g, '');
+  lastChar = trimmedDisplay.charAt(trimmedDisplay.length - 1);
+
+  //ADDS NUMBER TO DISPLAY
+  if (!(num == "." && lastChar == ".")) {
+    currDisplay.innerHTML = currDisplay.innerHTML + num;
+  }
+}
+
+export function operatorInp(op) {
+  currDisplay = document.getElementById("display");
+
+  wipeable = false;
+
+  //WIPES DISPLAY
+  if (wipeList.includes(currDisplay.innerHTML) && op == "-") {
+    currDisplay.innerHTML = "";
+  }
+
+  //GETS LAST CHARACTER
+  let trimmedDisplay = currDisplay.innerHTML.replace(/\s/g, '');
+  lastChar = trimmedDisplay.charAt(trimmedDisplay.length - 1);
+
+  //ADDS OPERATOR TO DISPLAY
+  if ((op == "-") && ((possibleOperators.includes(lastChar) || currDisplay.innerHTML == ""))) {
+    currDisplay.innerHTML = currDisplay.innerHTML + " " + op;
+  } else if ((isNaN(lastChar) == false || nonNumExceptions.includes(lastChar)) && currDisplay.innerHTML != "") {
+    currDisplay.innerHTML = currDisplay.innerHTML + " " + op + " ";
+  }
+}
+
+export function brackInp (brack) { 
+  currDisplay = document.getElementById("display");
+
+    //WIPES DISPLAY
+    if (wipeable == true) {
+      clearDisplay();
+      wipeable = false;
+    }
+
+    //WIPES DISPLAY
+    if (wipeList.includes(currDisplay.innerHTML)) {
+      currDisplay.innerHTML = "";
+    }
+
+    //ADDS BRACKET TO DISPLAY
+    currDisplay.innerHTML = currDisplay.innerHTML + " " + brack + " ";
+}
+
+export function evalInp() {
+  currDisplay = document.getElementById("display");
+
+  //REMOVES EMPTY STRINGS FROM ARRAY
+  let calcMemory = currDisplay.innerHTML.trim().split(" ");
+  calcMemory = calcMemory.filter(function (el) {
+    return el != "";
+  });
+
+  //ASSIGNMENTS 
+  let brackL = 0;
+  let brackR = 0;
+  let i = -1;
+  let errorPresent = false;
+
+  //WHILE LOOP THAT STOPS WHEN THERE'S NO MORE BRACKETS OR ERROR
+  while (calcMemory.includes("(") && (errorPresent == false)) {
+    i++;
+    //GETS POS OF LEFT BRACKET THAT'S CLOSEST TO A RIGHT ONE
+    if (calcMemory[i] == "(") {
+      brackL = i;
+      //GETS POS OF FIRST RIGHT BRACKET AND DOES CALCULATIONS
+    } else if (calcMemory[i] == ")") {
+      brackR = i;
+      //ARRAY IS CREATED TO PERFORM EDMAS ON THE VALUES INSIDE THE BRACKETS UNTIL IT'S REDUCED TO SINGLE VALUE
+      tempArray = calcMemory.slice(brackL + 1, brackR);
+      let deleteLength = brackR - brackL + 1
+      let finVal = doEDMAS(tempArray);
+
+      calcMemory.splice(brackL, deleteLength, finVal);
+
+      //STARTS SEARCH FOR BRACKETS AGAINT FROM ZERO
+      i = -1;
+    }
+
+    //DISPLAYS ERROR WHEN THERE'S AN INFITE LOOP
+    if (i > 99) {
+      errorPresent = true;
+    }
+
+  }
+
+  //DISPLAYS FLOAT OR INTEGER
+  if (doEDMAS(calcMemory).includes(".") == true) {
+    currDisplay.innerHTML = parseFloat(Number(doEDMAS(calcMemory)).toFixed(9));
+  } else {
+    currDisplay.innerHTML = doEDMAS(calcMemory);
+  }
+
+  //IF A NON-NUMBER IS PRODUCED, ERROR IS SHOWN
+  if (isNaN(currDisplay.innerHTML) == true) {
+    currDisplay.innerHTML = "ERROR"
+  }
+
+  wipeable = true;
+}
+
+export function clearDisplay() {
+  document.getElementById("display").innerHTML = "0";
+}
+
+export function backspace() {
+  let currDisplay = document.getElementById("display");
+
+  //GETS LAST CHAR
+  let trimmedDisplay = currDisplay.innerHTML.replace(/\s/g, '');
+  lastChar = trimmedDisplay.charAt(trimmedDisplay.length - 1);
+
+  //TRIMS DISPLAY IF THERE'S A SPACE
+  if (lastChar = " ") {
+    currDisplay.innerHTML = currDisplay.innerHTML.trim();
+  }
+  //WIPES DISPLAY
+  if (wipeable == true) {
+    clearDisplay();
+    wipeable = false;
+  }
+
+  //WIPES DISPLAY
+  if (wipeList.includes(currDisplay.innerHTML)) {
+    currDisplay.innerHTML = "0";
+  }
+
+  //REMOVES LAST CHARACTER
+  console.log(currDisplay.innerlength)
+  if (currDisplay.innerHTML.length == 1) {
+    currDisplay.innerHTML = "0";
+  } else if (currDisplay.innerHTML != "0") {
+    currDisplay.innerHTML = currDisplay.innerHTML.slice(0, currDisplay.innerHTML.length - 1);
+  }
+
+}
